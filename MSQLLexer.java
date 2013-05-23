b9418cbf17d02297857d901f88e9945f239a7295
xf <xfygj@126.com>
2013-05-23 11:28:45 +0800

    master的改动1

--- MSQLLexer.java
+++ MSQLLexer.java
@@ -23,6 +23,9 @@ public class MSQLLexer {
 		idxstart = sql.indexOf('{', idxstart);
 		if (idxstart <0)
 			return null;
+			
+			
+			//master comment
 		
 		idxstart = idxstart + 1;
 		int idxend = sql.indexOf('}', idxstart);
